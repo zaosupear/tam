7040def805f94942ff4b73083c78cfc097072cbe
zaosupear <1206630945@qq.com>
2020-12-28 10:15:09 +0800

    将公用相应类数据类型变为泛型

--- src/main/java/com/rango/tam/common/Result.java
+++ src/main/java/com/rango/tam/common/Result.java
@@ -1,5 +1,6 @@
 package com.rango.tam.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -10,39 +11,40 @@ import java.io.Serializable;
  * @date 2020-12-27 22:26
  */
 @Data
-public class Result implements Serializable {
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+public class Result<T> implements Serializable {
 
     private Integer code;
     private String message;
-    private Object data;
+    private T data;
     private ResultCode resultCode;
 
-    public Result(ResultCode resultCode, Object data){
+    public Result(ResultCode resultCode, T data){
         this.code = resultCode.code();
         this.message = resultCode.message();
         this.data = data;
     }
 
-    public static Result success(){
+    public static <T> Result<T> success(){
         Result result = new Result();
         result.setResultCode(ResultCode.SUCCESS);
         return result;
     }
 
-    public static Result success(Object data){
+    public static <T> Result<T> success(T data){
         Result result = new Result();
         result.setResultCode(ResultCode.SUCCESS);
         result.setData(data);
         return result;
     }
 
-    public static Result failure(ResultCode resultCode){
+    public static <T> Result<T> failure(ResultCode resultCode){
         Result result = new Result();
         result.setResultCode(resultCode);
         return result;
     }
 
-    public static Result failure(ResultCode resultCode, Object data){
+    public static <T> Result<T> failure(ResultCode resultCode, T data){
         Result result = new Result();
         result.setResultCode(resultCode);
         result.setData(data);
