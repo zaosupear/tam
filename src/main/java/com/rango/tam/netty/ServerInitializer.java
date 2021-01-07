4a714774d32312810a446fd9cbf7f007623398fd
rango <1206630945@qq.com>
2021-01-07 22:38:57 +0800

    修改为netty网上聊天的代码

--- src/main/java/com/rango/tam/netty/ServerInitializer.java
+++ src/main/java/com/rango/tam/netty/ServerInitializer.java
@@ -3,7 +3,10 @@ package com.rango.tam.netty;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * @author rango
@@ -15,7 +18,10 @@ public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
-        pipeline.addLast("HttpServerCodec", new HttpServerCodec());
-        pipeline.addLast("SystemHandler", new SystemHandler());
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(1024*64));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new SystemHandler());
     }
 }
--- src/main/java/com/rango/tam/netty/SystemHandler.java
+++ src/main/java/com/rango/tam/netty/SystemHandler.java
@@ -5,29 +5,31 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 /**
  * @author rango
  * @description
  * @date 2021-01-03 18:39
  */
-public class SystemHandler extends SimpleChannelInboundHandler<HttpObject> {
+public class SystemHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject)
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame)
             throws Exception {
+        clients.writeAndFlush(new TextWebSocketFrame(textWebSocketFrame.text()));
+    }
 
-        Channel channel = channelHandlerContext.channel();
-        if (httpObject instanceof HttpRequest){
-            System.out.println(channel.remoteAddress());
-            ByteBuf byteBuf = Unpooled.copiedBuffer("Hello netty ~", CharsetUtil.UTF_8);
-            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
-            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain")
-                    .set(HttpHeaderNames.CONTENT_LENGTH, byteBuf.readableBytes());
-            channelHandlerContext.writeAndFlush(response);
-        }
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        clients.add(ctx.channel());
     }
 
 }
