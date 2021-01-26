3743211f51a68ba3dc2c1bae76dc876ac85d0fc0
zaosupear <1206630945@qq.com>
2021-01-26 15:07:31 +0800

    增加底层相关表创建语句

--- src/main/resources/data/sql/tam.sql
+++ src/main/resources/data/sql/tam.sql
@@ -1,69 +1,133 @@
-sys_usergroup:
-id bigint 用户组ID
-usergroup_name varchar(50) 用户组名称
-parent_id bigint 父用户组名称
-create_by bigint 创建人ID
-create_name varchar(30) 创建人
-create_date datatime 创建时间
-update_by bigint 更新人ID
-update_name varchar(30) 更新人
-update_date datatime 更新时间
-deleted int 逻辑删除-旗标（1：删除 0：未删除）
+/*
+ Navicat Premium Data Transfer
 
-sys_user_usergroup:
-usergroup_id bigint 用户组ID
-user_id bigint 用户ID
+ Source Server         : MySQL-Local
+ Source Server Type    : MySQL
+ Source Server Version : 50730
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : tam
 
-sys_user:
-id bigint 用户ID
-username varchar(30) 用户名
-password varchar(50) 密码
-mobilephone varchar(30) 手机号
-email varchar(50) 邮箱
-enabled int 账号是否可用（1：可用 0：不可用）
-account_expired 账号是否过期（1：过期 0：未过期）
-credentials_expired 凭证是否过期（1：过期 0：未过期）
-account_locked 账号是否锁定（1：锁定 0：未锁定）
-create_by bigint 创建人ID
-create_name varchar(30) 创建人
-create_date datatime 创建时间
-update_by bigint 更新人ID
-update_name varchar(30) 更新人
-update_date datatime 更新时间
-deleted int 逻辑删除-旗标（1：删除 0：未删除）
+ Target Server Type    : MySQL
+ Target Server Version : 50730
+ File Encoding         : 65001
 
-sys_user_role:
-user_id bigint 用户ID
-role_id bigint 角色ID
+ Date: 26/01/2021 15:05:12
+*/
 
-sys_usergroup_role:
-usergroup_id bigint 用户组ID
-role_id bigint 角色ID
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-sys_role:
-id bigint 角色ID
-role_code varchar(18) 角色编码
-role_name varchar(50) 角色名
-create_by bigint 创建人ID
-create_name varchar(30) 创建人
-create_date datatime 创建时间
-update_by bigint 更新人ID
-update_name varchar(30) 更新人
-update_date datatime 更新时间
-deleted int 逻辑删除-旗标（1：删除 0：未删除）
+-- ----------------------------
+-- Table structure for sys_privilege
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_privilege`;
+CREATE TABLE `sys_privilege`  (
+  `id` bigint(32) NOT NULL COMMENT '权限ID',
+  `privilege_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限标识',
+  `create_by` bigint(32) NULL DEFAULT NULL COMMENT '创建人ID',
+  `create_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint(32) NULL DEFAULT NULL COMMENT '更新人ID',
+  `update_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新人',
+  `update_date` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `deleted` int(1) NULL DEFAULT NULL COMMENT '逻辑删除-旗标（1：删除 0：未删除）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-sys_role_privilege:
-role_id bigint 角色ID
-privilege_id bigint 权限ID
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` bigint(32) NOT NULL COMMENT '角色ID',
+  `role_code` varchar(18) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色编码',
+  `role_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
+  `create_by` bigint(32) NULL DEFAULT NULL COMMENT '创建人ID',
+  `create_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint(32) NULL DEFAULT NULL COMMENT '更新人ID',
+  `update_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新人',
+  `update_date` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `deleted` int(1) NULL DEFAULT NULL COMMENT '逻辑删除-旗标（1：删除 0：未删除）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-sys_privilege:
-id bigint 权限ID
-privilege_name varchar(50) 权限标识
-create_by bigint 创建人ID
-create_name varchar(30) 创建人
-create_date datatime 创建时间
-update_by bigint 更新人ID
-update_name varchar(30) 更新人
-update_date datatime 更新时间
-deleted int 逻辑删除-旗标（1：删除 0：未删除）
+-- ----------------------------
+-- Table structure for sys_role_privilege
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_privilege`;
+CREATE TABLE `sys_role_privilege`  (
+  `role_id` bigint(32) NOT NULL COMMENT '角色ID',
+  `privilege_id` bigint(32) NOT NULL COMMENT '权限ID'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` bigint(32) NOT NULL COMMENT '用户ID',
+  `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `mobilephone` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `enabled` int(1) NULL DEFAULT NULL COMMENT '账号是否可用（1：可用 0：不可用）',
+  `account_expired` int(1) NULL DEFAULT NULL COMMENT '账号是否过期（1：过期 0：未过期）',
+  `credentials_expired` int(1) NULL DEFAULT NULL COMMENT '凭证是否过期（1：过期 0：未过期）',
+  `account_locked` int(1) NULL DEFAULT NULL COMMENT '账号是否锁定（1：锁定 0：未锁定）',
+  `create_by` bigint(32) NULL DEFAULT NULL COMMENT '创建人ID',
+  `create_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint(32) NULL DEFAULT NULL COMMENT '更新人ID',
+  `update_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新人',
+  `update_date` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `deleted` int(1) NULL DEFAULT NULL COMMENT '逻辑删除-旗标（1：删除 0：未删除）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`  (
+  `user_id` bigint(32) NOT NULL COMMENT '用户ID',
+  `role_id` bigint(32) NOT NULL COMMENT '角色ID'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user_usergroup
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_usergroup`;
+CREATE TABLE `sys_user_usergroup`  (
+  `usergroup_id` bigint(32) NOT NULL,
+  `user_id` bigint(32) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_usergroup
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_usergroup`;
+CREATE TABLE `sys_usergroup`  (
+  `id` bigint(32) NOT NULL COMMENT '用户组ID',
+  `usergroup_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户组名称',
+  `parent_id` bigint(32) NULL DEFAULT NULL COMMENT '父用户组名称',
+  `create_by` bigint(32) NULL DEFAULT NULL COMMENT '创建人ID',
+  `create_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint(32) NULL DEFAULT NULL COMMENT '更新人ID',
+  `update_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新人',
+  `update_date` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `deleted` int(1) NULL DEFAULT NULL COMMENT '逻辑删除-旗标（1：删除 0：未删除）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_usergroup_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_usergroup_role`;
+CREATE TABLE `sys_usergroup_role`  (
+  `usergroup_id` bigint(32) NOT NULL COMMENT '用户组ID',
+  `role_id` bigint(32) NOT NULL COMMENT '角色ID'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
